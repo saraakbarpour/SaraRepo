c5ae86cbde34300fc4648424f1ab98a1813f7db5
Leo Liu <leo.liu@planetinnovation.com.au>
2018-05-19 10:44:24 +1000

    Included the uri to account creation page.

---
+++ WebUI.Automation.Pages/AccountCreation/CreateAccountPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Automation.Core.SeleniumUtility;
+
+namespace WebUI.Automation.Pages.AccountCreation
+{
+	public class CreateAccountPage : BasePage
+	{
+		public CreateAccountPage(IExtendedWebDriver webDriver, Options options) : base(webDriver)
+		{
+			PageUrl = new Uri(options.SiteUri, "Account/Create").ToString();
+		}
+	}
+}
